983fc0635f8ee3313bbc224a8895acadc075b306
ibeckermayer <ibeckermayer@gmail.com>
2017-12-02 13:22:18 -0800

    working, now going to attempt to implement fft

--- visualizer/visualizer.ino
+++ visualizer/visualizer.ino
@@ -1,5 +1,6 @@
 const int sensorPin = A0; // select the input pin for the potentiometer
 int sensorValue = 0; // variable to store the value coming from the sensor
+const int est_0_val = 840; //the estimated value to subtract so that no sound = 0 (determined empirically)
 
 
 void setup() {
@@ -9,5 +10,8 @@ void setup() {
 
 void loop() {
   sensorValue = analogRead(sensorPin);
-  Serial.println(sensorValue);
+  int trueValue = sensorValue-est_0_val; //estimated value with no sound = 0
+  Serial.println(trueValue);
+//  Serial.print(",");
+//  Serial.println(sensorValue);
 }
